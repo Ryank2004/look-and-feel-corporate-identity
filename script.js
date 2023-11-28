424d11c6e1842d2caab0732d5a1ee586b0ff5cf3
koeenm <mkoeen2@gmail.com>
2023-11-28 16:00:23 +0100

    engelse naamgeving

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function setActive(section) {
-    var aangeboden = document.querySelector('.aangeboden');
-    var gezocht = document.querySelector('.gezocht');
+    var offered = document.querySelector('.offered');
+    var wanted = document.querySelector('.wanted');
 
-    if (section === 'aangeboden') {
-        aangeboden.classList.add('active');
-        gezocht.classList.remove('active');
-    } else if (section === 'gezocht') {
-        gezocht.classList.add('active');
-        aangeboden.classList.remove('active');
+    if (section === 'offered') {
+        offered.classList.add('active');
+        wanted.classList.remove('active');
+    } else if (section === 'wanted') {
+        wanted.classList.add('active');
+        offered.classList.remove('active');
     }
 }
 
